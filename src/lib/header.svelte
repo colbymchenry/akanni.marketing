e4310a1f3ada4f8f1e7c74bfec7a880bc571f1a9
Colby <me@colbymchenry.com>
2022-08-24 18:04:44 -0400

    Header animation.

--- src/lib/header.svelte
+++ src/lib/header.svelte
@@ -1,4 +1,25 @@
-<header>
+<script>
+
+    let hidden = false;
+
+    let lastScrollTop = 0;
+
+
+    function handleScroll(e) {
+        let st = document.querySelector("BODY").scrollTop;
+        if (st > lastScrollTop){
+            hidden = true;
+        } else {
+            hidden = false;
+        }
+        lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
+    }
+
+</script>
+
+<svelte:body on:scroll={handleScroll} />
+
+<header class:is--hidden={hidden} class:is--opaque={lastScrollTop > 10}>
     <img src="/logos/purple.png" alt=""/>
 </header>
 
@@ -8,15 +29,24 @@
     top: 0;
     left: 0;
     width: 100%;
-    height: 3.25rem;
+    height: 4.25rem;
     padding: 1rem 2rem;
     transition: box-shadow 0.4s cubic-bezier(0.77, 0, 0.175, 1) 0.05s, background-color 0.4s cubic-bezier(0.77, 0, 0.175, 1) 0.05s, margin-top 0.3s cubic-bezier(0.39, 0.575, 0.565, 1);
     display: flex;
     align-items: center;
     background-color: transparent;
+    z-index: 10;
 
     img {
       max-height: 100%;
+    }
+
+    &.is--hidden {
+      margin-top: -3.5rem;
+    }
+
+    &.is--opaque {
+      background-color: rgba(10, 10, 15, 0.96);
 
     }
   }
