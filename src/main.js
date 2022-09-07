8c52e9678eaac62d549c46008e8d69812c4d2f49
Colby <me@colbymchenry.com>
2022-09-07 10:13:45 -0400

    Firebase hosting config files.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,23 @@
 import './app.css'
 import App from './App.svelte'
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCRVmWRrOKb8iFaBnvVwdg6cUoU_DEUbWI",
+  authDomain: "akanni-marketing.firebaseapp.com",
+  projectId: "akanni-marketing",
+  storageBucket: "akanni-marketing.appspot.com",
+  messagingSenderId: "824891637147",
+  appId: "1:824891637147:web:c806014b07ad5c6259b53e",
+  measurementId: "G-MESLK43KKL"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+const analytics = getAnalytics(firebaseApp);
+
 
 const app = new App({
   target: document.getElementById('app')
